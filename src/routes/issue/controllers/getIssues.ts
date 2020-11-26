f9f4a02a0d155812348c32bcf84bf60df44e13c3
HojinLee <hojin5633@gmail.com>
2020-11-26 14:21:08 +0900

    [fix] issues api stack 수정
    - issues api stack 수정
      stack에 첫 번째 stack 정보만 들어있도록 수정.

--- src/routes/issue/controllers/getIssues.ts
+++ src/routes/issue/controllers/getIssues.ts
@@ -2,7 +2,9 @@ import { Context, Next } from 'koa';
 import Issue, { IssueDocument } from '../../../models/Issue';
 
 export default async (ctx: Context, next: Next): Promise<void> => {
-  const result: IssueDocument[] = await Issue.find();
+  const result: IssueDocument[] = await Issue.aggregate([
+    { $addFields: { stack: { $arrayElemAt: ['$stack', 0] } } },
+  ]);
   ctx.body = result;
 
   await next();
