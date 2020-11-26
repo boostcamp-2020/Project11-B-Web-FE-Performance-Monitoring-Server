ad5dbfd6fb5bdf92d9b87b01032fee18d2d34fe5
juyoungpark718 <juyoung7018.park@gmail.com>
2020-11-26 09:16:14 +0900

    [refactor] return타입이 없는 것들을 추가, IssueDocument 타입으로 수정

--- src/routes/issue/controllers/getIssues.ts
+++ src/routes/issue/controllers/getIssues.ts
@@ -1,8 +1,8 @@
 import { Context, Next } from 'koa';
-import Issue, { IssueTypeModel } from '../../../models/Issue';
+import Issue, { IssueDocument } from '../../../models/Issue';
 
-export default async (ctx: Context, next: Next) => {
-  const result: IssueTypeModel[] = await Issue.find();
+export default async (ctx: Context, next: Next): Promise<void> => {
+  const result: IssueDocument[] = await Issue.find();
   ctx.body = result;
 
   await next();
