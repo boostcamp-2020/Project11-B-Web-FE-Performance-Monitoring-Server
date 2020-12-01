e9dc1b214715ec412d089289df3ddc66b89e4608
HojinLee <hojin5633@gmail.com>
2020-12-01 23:50:23 +0900

    [fix] OAuth scope 수정
    - github OAuth scope 수정
       사용자의 email을 받아올 수 있도록 수정
       사용자가 public email을 등록하지 않으면 null이므로 모델은 일단 변경하지 않음

--- src/routes/auth/controllers/github.ts
+++ src/routes/auth/controllers/github.ts
@@ -4,7 +4,7 @@ require('dotenv').config();
 
 export default async (ctx: Context, next: Next): Promise<void> => {
   ctx.redirect(
-    `https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_OAUTH_CLIENT_ID}`,
+    `https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_OAUTH_CLIENT_ID}&scope=user:email`,
   );
   await next();
 };
