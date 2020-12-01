b1eec65ba0cb79064b3e6d381bd549af78a61f09
HojinLee <hojin5633@gmail.com>
2020-12-01 18:54:51 +0900

    [fix] cookie 저장 옵션 httpOnly false로 변경
    - cookie 저장 옵션을 httpOnly로 변경
      true일 경우 Admin의 브라우저에서 토큰 값을 읽지 못함
      (React가 로그인 판별 불가)
    
    - 로그인 방식에 대해 추후 논의 필요

--- src/routes/auth/controllers/githubCallback.ts
+++ src/routes/auth/controllers/githubCallback.ts
@@ -9,10 +9,15 @@ const tokenExpiration: number = 3 * HOUR;
 
 export default async (ctx: Context, next: Next): Promise<void> => {
   const accessCode: string = ctx.query.code;
-  const newUser: UserDocument | null = await processGithubOAuth(accessCode);
+  const newUser: UserDocument = (await processGithubOAuth(accessCode)) as UserDocument;
   const jwtToken: string | undefined = newUser ? getToken(newUser, tokenExpiration) : undefined;
   if (jwtToken) {
-    ctx.cookies.set('token', jwtToken, { httpOnly: true, maxAge: tokenExpiration });
+    // ctx.cookies.set('nickname', newUser.nickname, { httpOnly: false, maxAge: tokenExpiration });
+    ctx.cookies.set('token', jwtToken, { httpOnly: false, maxAge: tokenExpiration });
+    /**
+     * @TODO
+     * http only option
+     */
   }
   ctx.redirect(process.env.ADMIN_MAIN_URL as string);
   await next();
