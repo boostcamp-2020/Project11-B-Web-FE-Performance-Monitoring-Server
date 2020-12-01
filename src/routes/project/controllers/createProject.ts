6e4906083f9b2dc589013d10d4c17a66bcbd6293
juyoungpark718 <juyoung7018.park@gmail.com>
2020-12-01 15:13:42 +0900

    [feat] createProject 컨트롤러 구현

---
+++ src/routes/project/controllers/createProject.ts
@@ -0,0 +1,16 @@
+import { Context, Next } from 'koa';
+import Project, { IProject, ProjectDocument } from '../../../models/project';
+
+export default async (ctx: Context, next: Next): Promise<void> => {
+  const newProject: IProject = ctx.request.body;
+  newProject.users = [];
+  try {
+    const newProjectDoc: ProjectDocument = Project.build(newProject);
+    await newProjectDoc.save();
+    ctx.response.status = 200;
+  } catch (e) {
+    ctx.throw(400, 'validation failed');
+  }
+
+  await next();
+};
