a9d8cf1ca68c059ef66fd035621d2d5d93ff1d8f
juyoungpark718 <juyoung7018.park@gmail.com>
2020-12-02 14:09:43 +0900

    [feat] User에 projects column추가, document에 method 추가
    - document에 addProject, deleteProject 추가

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,9 +1,10 @@
-import { Schema, Document, model, Model } from 'mongoose';
+import { Schema, Document, model, Model, Types } from 'mongoose';
 
 export interface UserType {
   uid: number;
   nickname: string;
   email: string | undefined;
+  projects: string[];
 }
 
 export interface UserDocument extends UserType, Document {}
@@ -15,10 +16,19 @@ const userSchema = new Schema({
   uid: { type: Number, required: true },
   nickname: { type: String, required: true },
   email: { type: String },
+  projects: { type: Types.Array, required: true },
 });
 
 userSchema.statics.build = function buildUser(user: UserType): UserDocument {
   return new this(user);
+};
+
+userSchema.methods.addProject = function addProject(projectId: string) {
+  this.projects.push(projectId);
+};
+
+userSchema.methods.deleteProject = function deleteProject(projectId: string) {
+  this.projects.pull(projectId);
 };
 
 const User = model<UserDocument, UserModel>('User', userSchema);
