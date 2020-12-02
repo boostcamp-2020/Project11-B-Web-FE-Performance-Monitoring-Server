584019e4a1ff41657cf4c9c07c3e105f817ceb60
saeeng <saeeng.opt@gmail.com>
2020-12-02 11:11:47 +0900

    [fix] sendmail 유틸 함수 수정
    - promise 제거
    - 변수값 수정

--- src/routes/test/controllers/sendMail.ts
+++ src/routes/test/controllers/sendMail.ts
@@ -1,14 +1,13 @@
 import { Context, Next } from 'koa';
-
 import sendMail from '../../../utils/sendMail';
 
 export default async (ctx: Context, next: Next): Promise<void> => {
-  const info = await sendMail(
-    ['saeeng.opt@gmail.com', 'dinero8807@naver.com'], // list of receivers
-    'Hello ✔', // Subject line
-    'Hello world?', // plain text body
-    '<div>Hello world?!!!!!!!!!!</div>', // html body
-  );
+  const info = await sendMail({
+    to: ['saeeng.opt@gmail.com'], // list of receivers
+    subject: 'Hello ✔', // Subject line
+    text: 'Hello world?', // plain text body
+    html: '<b>Hello world?</b>',
+  });
   ctx.body = info;
   await next();
 };
--- src/utils/sendMail.ts
+++ src/utils/sendMail.ts
@@ -1,34 +1,30 @@
 import nodemailer from 'nodemailer';
 
-const sendMail = async (
-  _to: string[],
-  _subject: string,
-  _text: string,
-  _html: string,
-): Promise<void> => {
-  return new Promise((resolve, reject) => {
-    // code goes here
-    try {
-      const mailer = nodemailer.createTransport({
-        service: 'gmail',
-        auth: {
-          user: process.env.MAILER_ID, // generated ethereal user
-          pass: process.env.MAILER_PASSWORD, // generated ethereal password
-        },
-      });
+const sendMail = async (params: {
+  to: string[];
+  subject: string;
+  text: string;
+  html: string;
+}): Promise<void> => {
+  const { to, subject, text, html } = params;
 
-      const info = mailer.sendMail({
-        from: process.env.MAILER_ID, // sender address
-        to: _to, // list of receivers
-        subject: _subject, // Subject line
-        text: _text, // plain text body
-        html: _html, // html body
-      });
-      resolve(info);
-    } catch (err) {
-      reject(err);
-    }
+  const mailer = await nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: process.env.MAILER_ID, // generated ethereal user
+      pass: process.env.MAILER_PASSWORD, // generated ethereal password
+    },
   });
+
+  const info = await mailer.sendMail({
+    from: process.env.MAILER_ID, // sender address
+    to, // list of receivers
+    subject, // Subject line
+    text, // plain text body
+    html, // html body
+  });
+
+  return info;
 };
 
 export default sendMail;
