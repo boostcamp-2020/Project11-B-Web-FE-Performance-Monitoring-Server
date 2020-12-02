b2bc51f3222351b416255907682363d14f3abb5d
HojinLee <hojin5633@gmail.com>
2020-12-02 10:40:09 +0900

    [fix] Github 로그인 Cancel시 처리
    - Github 로그인 Cancel 버튼을 누를 시 처리
       Cancel을 누를 시 오는 Error를 체크 후 메인 페이지로 리다이렉팅

--- src/routes/auth/controllers/githubCallback.ts
+++ src/routes/auth/controllers/githubCallback.ts
@@ -6,6 +6,11 @@ const HOUR: number = 1000 * 60 * 60;
 const tokenExpiration: number = 3 * HOUR;
 
 export default async (ctx: Context, next: Next): Promise<void> => {
+  if (ctx.query) {
+    ctx.redirect(process.env.ADMIN_MAIN_URL as string);
+    await next();
+    return;
+  }
   const accessCode: string = ctx.query.code;
   const newUser: UserDocument = (await processGithubOAuth(accessCode)) as UserDocument;
   const jwtToken: string = getToken(newUser, tokenExpiration);
