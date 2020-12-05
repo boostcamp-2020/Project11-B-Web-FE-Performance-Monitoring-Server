2ed71264f34dd173d1252befb2dad595c2c6ee7c
EarlyHail <hojin5633@gmail.com>
2020-12-05 21:52:24 +0900

    [feat] get user api 추가
    - user 정보를 받아오는 api 추가

---
+++ src/routes/user/router.ts
@@ -0,0 +1,13 @@
+import Router from 'koa-router';
+
+import controllers from './controllers';
+
+export default async (): Promise<Record<string, unknown>> => {
+  const router = new Router();
+  const controller: any = await controllers();
+
+  //   get
+  router.get('/user/:id', controller.getUser);
+
+  return router;
+};
