7c3312a0cb4be32fbaf91b6fcc3aa9118d4c6f79
junsushin-dev <junsushin4546@gmail.com>
2020-12-06 18:05:51 +0900

    [refactor] 테스트 라우터, 모델 삭제 처리
    - 초반 세팅 시 예시용으로 생성하였으나 필요없으므로 삭제 처리함

--- src/routes/test/controllers/getTest.ts
+++
@@ -1,9 +0,0 @@
-import { Context, Next } from 'koa';
-import Test, { TestType, TestTypeModel } from '../../../models/Test';
-
-export default async (ctx: Context, next: Next) => {
-  const result = await Test.find();
-  ctx.body = result;
-
-  await next();
-};
