843b61889d7898d7b30d8ffbee46fb735baa42c0
juyoungpark718 <juyoung7018.park@gmail.com>
2020-12-07 19:16:11 +0900

    [refactor] Project accept api에 transaction 추가

--- src/routes/project/controllers/acceptInvite.ts
+++ src/routes/project/controllers/acceptInvite.ts
@@ -1,5 +1,5 @@
+import Mongoose from 'mongoose';
 import { Context, Next } from 'koa';
-
 import CryptoJS from 'crypto-js';
 import Invite from '../../../models/Invite';
 import Project from '../../../models/Project';
@@ -8,31 +8,41 @@ import User, { IUserDocument } from '../../../models/User';
 export default async (ctx: Context, next: Next): Promise<void> => {
   const { key } = ctx.query;
   const decodeKey = decodeURIComponent(key);
+
   const result = await Invite.findOne({ key: decodeKey, expire: { $lte: new Date().getTime() } });
   if (result) {
+    const session = await Mongoose.startSession();
+    session.startTransaction();
     const bytes = CryptoJS.AES.decrypt(result.key, process.env.INVITE_SECRET_KEY as string);
     const decryptedData = JSON.parse(bytes.toString(CryptoJS.enc.Utf8));
-    const project = await Project.findOne({ _id: decryptedData.projectId });
-    const user = (await User.findOne({ _id: ctx.state.user._id })) as IUserDocument;
+    const project = await Project.findOne({ _id: decryptedData.projectId }, null, { session });
+    const user = (await User.findOne({ _id: ctx.state.user._id }, null, {
+      session,
+    })) as IUserDocument;
     if (!project) {
-      ctx.response.status = 404;
-      ctx.throw('project not found');
+      await session.abortTransaction();
+      session.endSession();
+      ctx.throw(404, 'project not found');
     }
     try {
       await project.addUser(ctx.state.user._id);
       user.addProject(project._id);
-      user.save();
-      project.save();
+      await user.save();
+      await project.save();
+      await session.commitTransaction();
+      session.endSession();
       ctx.response.status = 200;
-    } catch (error) {
-      ctx.response.status = 500;
+    } catch (e) {
+      await session.abortTransaction();
+      session.endSession();
+      ctx.throw(500);
     }
     /**
      * @todo
      * project에 맞는 issue로 redirect
      */
   } else {
-    ctx.response.status = 500;
+    ctx.throw(401, 'invite code expired');
   }
   await next();
 };
