52d8234d8b7e89feebc17a4efbd6f2fe44edc351
saeeng <saeeng.opt@gmail.com>
2020-12-08 09:53:48 +0900

    [feat] getIssue api 리팩토링
    - 단일 이슈 조회 api 리팩토링
    - getIssues api와 이슈 형식 맞춤
    - 유저 ip 및 crime정보 포함

--- src/routes/issue/controllers/getIssue.ts
+++ src/routes/issue/controllers/getIssue.ts
@@ -1,13 +1,64 @@
 import { Context, Next } from 'koa';
+import { Types } from 'mongoose';
 import Issue, { IIssueDocument } from '../../../models/Issue';
 
 export default async (ctx: Context, next: Next): Promise<void> => {
   const { issueId } = ctx.params;
-  const result: IIssueDocument | null = await Issue.findOne({ _id: issueId });
-  /**
-   * @TODO
-   * null 처리 필요
-   */
+  // const result: IIssueDocument | null = await Issue.findOne({ _id: issueId });
+  const [result]: any[] = await Issue.aggregate([
+    { $match: { _id: Types.ObjectId(issueId) } },
+    // { $match: { projectId: Types.ObjectId(projectId) } },
+    { $addFields: { stack: { $arrayElemAt: ['$stack', 0] } } },
+    {
+      $lookup: {
+        localField: 'projectId',
+        from: 'projects',
+        foreignField: '_id',
+        as: 'project',
+      },
+    },
+
+    { $addFields: { lastErrorId: { $arrayElemAt: ['$errorIds', 0] } } },
+    {
+      $lookup: {
+        localField: 'lastErrorId',
+        // localField: 'errorIds',
+        from: 'errors',
+        foreignField: '_id',
+        as: 'lastError',
+      },
+    },
+
+    { $unwind: '$lastError' },
+    {
+      $lookup: {
+        // localField: 'lastErrorId',
+        localField: 'errorIds',
+        from: 'errors',
+        foreignField: '_id',
+        as: 'totalError',
+      },
+    },
+
+    {
+      $group: {
+        _id: {
+          _id: '$_id',
+          meta: '$meta',
+          type: '$type',
+          message: '$message',
+          stack: '$stack',
+          lastError: '$lastError',
+          project: '$project',
+          errorIds: '$errorIds',
+        },
+        _stat: { $addToSet: { userIps: '$totalError.meta.ip' } },
+      },
+    },
+
+    // metadata 배열 해제
+  ]);
+
   ctx.body = result;
 
   await next();
