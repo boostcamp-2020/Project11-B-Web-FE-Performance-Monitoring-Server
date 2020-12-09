ede6a7ee0e96d7c9a813f56c7c2102b19eab67fc
EarlyHail <hojin5633@gmail.com>
2020-12-09 19:20:36 +0900

    [feat] visits 삽입 시 조건 추가
    - visits 삽입 시 조건 추가
      projectid, ip, date가 모두 동일한 document 검색 후 없을 경우만 삽입

--- src/routes/crime/controllers/addVisits.ts
+++ src/routes/crime/controllers/addVisits.ts
@@ -8,8 +8,14 @@ interface IParams {
 export default async (ctx: Context): Promise<void> => {
   const { projectId }: IParams = ctx.params;
   const { ip } = ctx.request;
-  const date = new Date(Date.now());
+  const today = new Date();
+  const date = new Date(today.getFullYear(), today.getMonth(), today.getDate());
   try {
+    const visited = await Visits.findOne({ projectId, ip, date });
+    if (visited !== null) {
+      ctx.response.status = 200;
+      return;
+    }
     const newVisitsDoc: IVisitsDocument = Visits.build({ projectId, ip, date });
     await newVisitsDoc.save();
     ctx.response.status = 200;
