d612067c6949f2cd579ee1fc8fa82c9095ecf85d
saeeng <saeeng.opt@gmail.com>
2020-12-10 11:06:32 +0900

    [feat] project 모델에 session 추가
    - 유저 체류시간, 페이지간 이동 분석을위한 session 필드 추가
    - session 데이터 삽입을 위한 addSession 추가

--- src/models/Project.ts
+++ src/models/Project.ts
@@ -1,16 +1,27 @@
 import { Types, Schema, Document, model, Model, ClientSession } from 'mongoose';
 
+export interface ISession {
+  ip: string;
+  sessionBase: string;
+  prevLocation: string;
+  presentLocation: string;
+  prevTime: number;
+  presentTime: number;
+  duration: number;
+}
 export interface IProject {
   name: string;
   description?: string;
   owner: string;
   users: string[];
+  sessions: ISession[];
 }
 
 export interface IProjectDocument extends IProject, Document {
   addUser(user: string): Promise<void>;
   deleteUser(user: string): Promise<void>;
   deleteUsers(userIds: string[]): Promise<void>;
+  addSession(session: ISession): Promise<void>;
 }
 
 export interface IProjectModel extends Model<IProjectDocument> {
@@ -23,6 +34,7 @@ const projectSchema = new Schema({
   owner: { type: Schema.Types.ObjectId, required: true, ref: 'User' },
   users: [{ type: Schema.Types.ObjectId, required: true, ref: 'User' }],
   isDeleted: { type: Boolean, required: true, default: false },
+  sessions: { type: Schema.Types.Array },
 });
 
 projectSchema.statics.build = function buildProject(project: IProject, session?: ClientSession) {
@@ -34,6 +46,9 @@ projectSchema.methods.addUser = async function addUser(userId: string) {
   if (!this.users.includes(userId)) {
     this.users.push(userId);
   }
+};
+projectSchema.methods.addSession = async function addSession(session: ISession) {
+  this.sessions.push(session);
 };
 
 projectSchema.methods.deleteUser = function deleteUser(userId: string) {
