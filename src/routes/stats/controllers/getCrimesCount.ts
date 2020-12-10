c815b66feb1181541d23a03e794dfee8e72a144a
juyoungpark718 <juyoung7018.park@gmail.com>
2020-12-10 21:41:51 +0900

    [feat] intervalType을 받게 수정
    - intervalType으로 24시간, 30일 두가지 경우를 할 수 있게 확장

--- src/routes/stats/controllers/getCrimesCount.ts
+++ src/routes/stats/controllers/getCrimesCount.ts
@@ -1,26 +1,32 @@
 import { Context } from 'koa';
-import { getCrimesCountAggregate, setDefaultGroup } from '../services/crimesCount';
+import {
+  getCrimesCountAggregate,
+  setDefaultGroup,
+  getDateByInterval,
+} from '../services/crimesCount';
 import Issue from '../../../models/Issue';
-
-const MILLISECOND_OF_TWELVE_HOUR = 3600 * 24 * 1000;
 
 interface IParams {
   issueId: string;
 }
 
+interface IQuery {
+  intervalType: string;
+}
+
 export default async (ctx: Context): Promise<void> => {
   const { issueId }: IParams = ctx.params;
-  const start = new Date(Date.now() - MILLISECOND_OF_TWELVE_HOUR);
-  const end = new Date(Date.now());
-  const INTERVAL_ONE_HOUR = 60 * 1000 * 60;
+  const { intervalType }: IQuery = ctx.query;
+  const { start, end, interval } = getDateByInterval(intervalType);
   try {
     const [result] = await Issue.aggregate(
-      getCrimesCountAggregate({ issueId, start, end, interval: INTERVAL_ONE_HOUR }),
+      getCrimesCountAggregate({ issueId, start, end, interval }),
     );
     const crimesByDate = setDefaultGroup({
       start,
-      interval: INTERVAL_ONE_HOUR,
+      interval,
       crimes: result.crimes,
+      intervalType,
     });
     ctx.response.body = { crimes: crimesByDate };
   } catch (e) {
