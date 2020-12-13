36433e9e7551634bf30edcdbc4d47db143dc25f1
saeeng <saeeng.opt@gmail.com>
2020-12-13 20:50:59 +0900

    [fix] crime에 커스톰 태그 들어가도록 수정
    - strict 모드 해제를 통해 custom tag 삽입

--- src/models/Crime.ts
+++ src/models/Crime.ts
@@ -24,41 +24,45 @@ export interface ICrime {
   };
 }
 
-export interface ICrimeDocument extends ICrime, Document {}
+// eslint-disable-next-line prettier/prettier
+export interface ICrimeDocument extends ICrime, Document { }
 export interface ICrimeModel extends Model<ICrimeDocument> {
   build(attr: ICrime): ICrimeDocument;
 }
 
-const CrimeSchema = new Schema({
-  message: { type: String, required: true },
-  type: { type: String, required: true },
-  stack: [
-    {
-      columnNo: { type: String, required: true },
-      lineNo: { type: String, required: true },
-      function: { type: String, required: true },
-      filename: { type: String, required: true },
-    },
-  ],
-  occuredAt: { type: Date, required: true },
-  projectId: { type: String, require: true },
-  sdk: {
-    name: { type: String, required: true },
-    version: { type: String, required: true },
-  },
-  meta: {
-    browser: {
+const CrimeSchema = new Schema(
+  {
+    message: { type: String, required: true },
+    type: { type: String, required: true },
+    stack: [
+      {
+        columnNo: { type: String, required: true },
+        lineNo: { type: String, required: true },
+        function: { type: String, required: true },
+        filename: { type: String, required: true },
+      },
+    ],
+    occuredAt: { type: Date, required: true },
+    projectId: { type: String, require: true },
+    sdk: {
       name: { type: String, required: true },
       version: { type: String, required: true },
     },
-    os: {
-      name: { type: String, required: true },
-      version: { type: String, required: true },
+    meta: {
+      browser: {
+        name: { type: String, required: true },
+        version: { type: String, required: true },
+      },
+      os: {
+        name: { type: String, required: true },
+        version: { type: String, required: true },
+      },
+      url: { type: String, required: true },
+      ip: { type: String, required: true },
     },
-    url: { type: String, required: true },
-    ip: { type: String, required: true },
   },
-});
+  { strict: false },
+);
 
 CrimeSchema.statics.build = function buildCrime(Crime: ICrime): ICrimeDocument {
   return new this(Crime);
