c6b779e39a2f604f6da4eae9bade320e820bfaa2
EarlyHail <hojin5633@gmail.com>
2020-12-14 18:00:02 +0900

    [feat] project user 삭제 시 user의 project도 삭제하도록 수정
    - project user 삭제 시 user의 project도 삭제하도록 수정
       - 세선 적용
       - `updateMany`와 `$pull`을 이용하여 각 user의 projects에서 projectId를 제거하도록 구현

--- src/routes/project/controllers/deleteProjectUsers.ts
+++ src/routes/project/controllers/deleteProjectUsers.ts
@@ -1,5 +1,7 @@
+import Mongoose from 'mongoose';
 import { Context } from 'koa';
 import Project from '../../../models/Project';
+import User from '../../../models/User';
 
 interface IQuery {
   id: string;
@@ -12,15 +14,29 @@ interface IBody {
 export default async (ctx: Context): Promise<void> => {
   const { id: projectId }: IQuery = ctx.params;
   const { userIds }: IBody = ctx.request.body;
+  const session = await Mongoose.startSession();
+  session.startTransaction();
   try {
-    const project = await Project.findOne({ _id: projectId });
+    const project = await Project.findOne({ _id: projectId }, null, { session });
+
+    await User.updateMany(
+      { _id: { $in: userIds } },
+      { $pull: { projects: projectId } },
+      {
+        session,
+      },
+    );
     if (project === null) {
       ctx.throw(500);
     }
-    project.deleteUsers(userIds);
-    project.save();
+    await project.deleteUsers(userIds);
+    await project.save();
+    await session.commitTransaction();
+    session.endSession();
     ctx.status = 200;
   } catch (e) {
+    await session.abortTransaction();
+    session.endSession();
     ctx.throw(400, 'internal server error');
   }
 };
