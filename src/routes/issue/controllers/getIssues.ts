bb432c156f77b390d01341bbe52081604eaf906d
EarlyHail <hojin5633@gmail.com>
2020-12-14 21:06:51 +0900

    [refactor] issue controller 에러 리팩토링
    - issue 조회 로직 분리
       issue 조회 로직이 너무 길기 때문에 service 계층으로 분리
    
    - issue controller 에러 리팩토링
       db 조회에서 `ctx.throw(400)`을 던지도록 리팩토링

--- src/routes/issue/controllers/getIssues.ts
+++ src/routes/issue/controllers/getIssues.ts
@@ -1,10 +1,9 @@
-import { Context, Next } from 'koa';
+import { Context } from 'koa';
 import { Types } from 'mongoose';
-import Issue, { IIssueDocument } from '../../../models/Issue';
+import { IIssueDocument } from '../../../models/Issue';
+import getIssues from '../services/getIssues';
 
-const CONTENT_PER_PAGE = 15;
-
-export default async (ctx: Context, next: Next): Promise<void> => {
+export default async (ctx: Context): Promise<void> => {
   let { projectId } = ctx.query;
   if (!Array.isArray(projectId)) {
     projectId = [projectId];
@@ -12,102 +11,11 @@ export default async (ctx: Context, next: Next): Promise<void> => {
   projectId = projectId.map((id: string) => {
     return Types.ObjectId(id);
   });
-
   const page: number = parseInt(ctx.query.page, 10) || 1;
-
-  const [result]: IIssueDocument[] = await Issue.aggregate([
-    { $match: { projectId: { $in: projectId } } },
-    { $addFields: { stack: { $arrayElemAt: ['$stack', 0] } } },
-    {
-      $lookup: {
-        from: 'projects',
-        let: { id: '$projectId' },
-        pipeline: [{ $match: { $expr: { $eq: ['$_id', '$$id'] } } }, { $project: { name: 1 } }],
-
-        as: 'project',
-      },
-    },
-
-    { $addFields: { lastCrimeId: { $arrayElemAt: ['$crimeIds', -1] } } },
-    {
-      $lookup: {
-        localField: 'lastCrimeId',
-        from: 'crimes',
-        foreignField: '_id',
-        as: 'lastCrime',
-      },
-    },
-
-    { $unwind: '$lastCrime' },
-    {
-      $lookup: {
-        localField: 'crimeIds',
-        from: 'crimes',
-        foreignField: '_id',
-        as: 'totalCrime',
-      },
-    },
-    { $unwind: '$totalCrime' },
-
-    {
-      $group: {
-        _id: {
-          _id: '$_id',
-          type: '$type',
-          message: '$message',
-          stack: '$stack',
-          lastCrime: '$lastCrime',
-          project: '$project',
-        },
-        crimeIds: { $addToSet: '$crimeIds' },
-        users: { $addToSet: '$totalCrime.meta.ip' },
-      },
-    },
-    { $unwind: '$crimeIds' },
-    {
-      $group: {
-        _id: '$_id._id',
-        type: { $addToSet: '$_id.type' },
-        message: { $addToSet: '$_id.message' },
-        stack: { $addToSet: '$_id.stack' },
-        project: { $addToSet: '$_id.project' },
-        crimeIds: { $addToSet: '$crimeIds' },
-        lastCrime: { $addToSet: '$_id.lastCrime' },
-        crimeCount: { $sum: { $size: '$crimeIds' } },
-        userCount: { $sum: { $size: '$users' } },
-      },
-    },
-    { $unwind: '$type' },
-    { $unwind: '$message' },
-    { $unwind: '$stack' },
-    { $unwind: '$project' },
-    { $unwind: '$project' },
-    { $unwind: '$crimeIds' },
-    { $unwind: '$lastCrime' },
-    { $addFields: { occuredAt: { $toDate: '$lastCrime.occuredAt' } } },
-
-    {
-      $sort: {
-        occuredAt: -1,
-        crimeCount: 1,
-        userCount: 1,
-        _id: 1,
-      },
-    },
-    {
-      $facet: {
-        metaData: [
-          { $count: 'total' },
-          { $addFields: { totalPage: { $ceil: { $divide: ['$total', CONTENT_PER_PAGE] } } } },
-          { $addFields: { page } },
-          { $addFields: { countPerPage: CONTENT_PER_PAGE } },
-        ],
-        data: [{ $skip: (page - 1) * CONTENT_PER_PAGE }, { $limit: CONTENT_PER_PAGE }],
-      },
-    },
-    { $unwind: '$metaData' },
-  ]);
-  ctx.body = result;
-
-  await next();
+  try {
+    const result: IIssueDocument[] = await getIssues(projectId, page);
+    ctx.body = result;
+  } catch (e) {
+    ctx.throw(400);
+  }
 };
