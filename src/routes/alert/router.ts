07c4d214d998feb959c0e11a23c05e0a968fad53
juyoungpark718 <juyoung7018.park@gmail.com>
2020-12-16 06:53:14 +0900

    [feat] getAlerts controller 구현 후 연결

--- src/routes/alert/router.ts
+++ src/routes/alert/router.ts
@@ -7,6 +7,7 @@ export default async (): Promise<Record<string, unknown>> => {
   const controller: any = await controllers();
 
   // post
+  router.post('/alerts', controller.getAlerts);
   router.post('/alert/:projectId', controller.addAlert);
 
   return router;
