8bdd3b3febab76c43386769e57840643d9adafc2
juyoungpark718 <juyoung7018.park@gmail.com>
2020-12-16 05:46:44 +0900

    [feat] alert.ts에 사용할 util 구현
    - alertlist를 가져오는 aggregate를 따로 빼줌
    - alertByCount, alertByPeriod는 거의 동일 -> 추후에 리펙토링
       - lastest이슈가 있는 경우와 없는 경우를 나눠줬음.
    - sendEmailByPeriod, sendEmailByCount
      - period는 시간 기준으로 issues를 찾음. 마지막으로 보낸 시간 + 주기가 현재 시간보다 작아야함.
      - count는 limit으로 가져온 이슈가 count와 동일한 경우에 sendEmail

---
+++ src/utils/alertUtil.ts
@@ -0,0 +1,142 @@
+import { Types } from 'mongoose';
+import Issue from '../models/Issue';
+import Alert from '../models/Alert';
+import sendMail from './sendMail';
+import { alertMailTemplateByCount, alertMailTemplateByPeriod } from './mailTemplate/alertMail';
+import { getPeriodByMillisec } from '../routes/stats/services/statUtil';
+
+const alertListAggregate = () => {
+  return [
+    {
+      $match: {
+        sendedAt: { $lte: new Date() },
+      },
+    },
+    {
+      $lookup: {
+        from: 'projects',
+        localField: 'projectId',
+        foreignField: '_id',
+        as: 'project',
+      },
+    },
+    {
+      $lookup: {
+        from: 'users',
+        localField: 'users',
+        foreignField: '_id',
+        as: 'users',
+      },
+    },
+    { $unwind: '$project' },
+    {
+      $project: {
+        'project._id': 1,
+        'project.name': 1,
+        'users.nickname': 1,
+        'users.email': 1,
+        period: 1,
+        count: 1,
+        sendedAt: 1,
+        lastestIssueId: 1,
+      },
+    },
+  ];
+};
+
+const sendEmailByCount = async (
+  alertId: string,
+  project: any,
+  lastestIssueId: string,
+  count: number,
+  userList: string[],
+): Promise<void> => {
+  const issues = await Issue.aggregate([
+    {
+      $match: {
+        $and: [{ projectId: Types.ObjectId(project._id) }, { _id: { $gt: lastestIssueId } }],
+      },
+    },
+    { $sort: { _id: -1 } },
+    { $limit: count },
+  ]);
+  if (issues.length === count) {
+    await sendMail({
+      to: userList,
+      ...alertMailTemplateByCount(project.name, issues),
+    });
+    await Alert.updateOne({ _id: alertId }, { $set: { lastestIssueId: issues[0]._id } });
+  }
+};
+
+const alertByCount = async (alert: any): Promise<void> => {
+  const { _id: alertId, lastestIssueId, count, users, project } = alert;
+  const userList = users.map((user: any) => user.email);
+  if (lastestIssueId) {
+    sendEmailByCount(alertId, project, lastestIssueId, count, userList);
+  } else {
+    const [lastestIssue] = await Issue.aggregate([
+      { $match: { projectId: Types.ObjectId(project._id) } },
+      { $sort: { _id: -1 } },
+      { $limit: 1 },
+    ]);
+    if (lastestIssue) {
+      sendEmailByCount(alertId, project, lastestIssue._id, count, userList);
+    }
+  }
+};
+
+const sendEmailByPeriod = async (
+  alertId: string,
+  project: any,
+  lastestIssueId: string,
+  sendedAt: Date,
+  now: Date,
+  period: string,
+  userList: string[],
+): Promise<void> => {
+  const date = new Date(sendedAt.getTime() + getPeriodByMillisec(period));
+  if (date.getTime() - now.getTime() <= 0) {
+    const issues = await Issue.aggregate([
+      {
+        $match: {
+          $and: [{ projectId: Types.ObjectId(project._id) }, { _id: { $gt: lastestIssueId } }],
+        },
+      },
+      { $sort: { _id: -1 } },
+      { $limit: 10 },
+    ]);
+
+    // 최근에 보낸 시간 + period가 현재 시각보다 작은 경우 -> 이메일을 보냄
+    if (issues.length) {
+      console.log('period : send Email - 최근 에러 10개');
+      await sendMail({
+        to: userList,
+        ...alertMailTemplateByPeriod(project.name, issues),
+      });
+      await Alert.updateOne(
+        { _id: alertId },
+        { $set: { sendedAt: date, lastestIssueId: issues[0]._id } },
+      );
+    }
+  }
+};
+
+const alertByPeriod = async (alert: any, now: Date): Promise<void> => {
+  const { _id: alertId, sendedAt, period, project, lastestIssueId, users } = alert;
+  const userList = users.map((user: any) => user.email);
+  if (lastestIssueId) {
+    sendEmailByPeriod(alertId, project, lastestIssueId, sendedAt, now, period, userList);
+  } else {
+    const [lastestIssue] = await Issue.aggregate([
+      { $match: { projectId: Types.ObjectId(project._id) } },
+      { $sort: { _id: -1 } },
+      { $limit: 1 },
+    ]);
+    if (lastestIssue) {
+      sendEmailByPeriod(alertId, project, lastestIssue._id, sendedAt, now, period, userList);
+    }
+  }
+};
+
+export { alertByCount, alertListAggregate, alertByPeriod };
