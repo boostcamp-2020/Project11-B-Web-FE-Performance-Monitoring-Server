963f975734d99dce0db4ac0b9f843e7984336bae
juyoungpark718 <juyoung7018.park@gmail.com>
2020-12-15 14:35:23 +0900

    [refactor] self값으로 들어오는 경우 로그인된 유저를 리턴

--- src/routes/user/controllers/getUser.ts
+++ src/routes/user/controllers/getUser.ts
@@ -1,12 +1,17 @@
-import { Context, Next } from 'koa';
+import { Context } from 'koa';
 import User from '../../../models/User';
+import checkToken from '../../../utils/checkToken';
 
 interface IParams {
   id: string;
 }
 
 export default async (ctx: Context): Promise<void> => {
-  const { id: userId }: IParams = ctx.params;
+  const { token } = ctx.headers;
+  const { id: userParams }: IParams = ctx.params;
+  let userId = '';
+  if (userParams !== 'self') userId = userParams;
+  else userId = checkToken(token);
   try {
     const result = await User.findById(userId);
     if (result === null) throw Error();
