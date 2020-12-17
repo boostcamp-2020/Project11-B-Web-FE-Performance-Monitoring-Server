20ea917e0f6d023c9f2172d4dd2869a4277f7f61
EarlyHail <hojin5633@gmail.com>
2020-12-17 18:07:41 +0900

    [fix] Session 시간 수정
    - mongoDB로 쿼리를 실행하기 때문에 Timezone이 맞지 않는 오류
      aggregate를 사용하여 mongDB쪽의 timezone을 변경해야하지만 방법이 없어서 Application 쪽에서 해결
      삽입할 때 9시간을 더해주는 방법으로 오류 수정

--- src/routes/sdk/controllers/addSession.ts
+++ src/routes/sdk/controllers/addSession.ts
@@ -2,13 +2,16 @@ import { Context } from 'koa';
 
 import Project, { IProjectDocument } from '../../../models/Project';
 
+const NINE_HOUR = 9 * 60 * 60 * 1000;
+
 export default async (ctx: Context): Promise<void> => {
   const params = ctx.request.body;
   const { projectId } = ctx.params;
   params.ip = ctx.request.ip;
   try {
     const project = (await Project.findById(projectId)) as IProjectDocument;
-
+    params.prevTime += NINE_HOUR;
+    params.presentTime += NINE_HOUR;
     project.addSession(params);
     project.save();
 
